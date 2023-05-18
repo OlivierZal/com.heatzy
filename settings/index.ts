d808a903f234b714812f510153574b208da7837d
Olivier Zalmanski <olivier.zalmanski@me.com>
2023-05-19 00:32:18 +0200

    Fix HTML

--- settings/index.ts
+++ settings/index.ts
@@ -154,15 +154,15 @@ async function onHomeyReady(Homey: Homey): Promise<void> {
       }
       const divElement: HTMLDivElement = document.createElement('div')
       divElement.classList.add('homey-form-group')
-      const inputElement: HTMLInputElement = document.createElement('input')
-      inputElement.classList.add('homey-form-input')
-      inputElement.id = setting.id
-      inputElement.type = setting.type
-      inputElement.placeholder = setting.placeholder ?? ''
-      inputElement.value = credentials[setting.id]
       const labelElement: HTMLLabelElement = document.createElement('label')
       labelElement.classList.add('homey-form-label')
       labelElement.innerText = setting.title
+      const inputElement: HTMLInputElement = document.createElement('input')
+      inputElement.classList.add('homey-form-input')
+      inputElement.type = setting.type
+      inputElement.placeholder = setting.placeholder ?? ''
+      inputElement.value = credentials[setting.id]
+      inputElement.id = setting.id
       labelElement.htmlFor = inputElement.id
       loginElement.appendChild(labelElement)
       loginElement.appendChild(inputElement)
