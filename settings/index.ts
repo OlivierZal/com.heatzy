3db78b9d4fc76ccc2cc7f0d2ae4fd32478a2e99d
Olivier Zalmanski <olivier.zalmanski@me.com>
2023-07-24 11:02:00 +0200

    Minor change

--- settings/index.ts
+++ settings/index.ts
@@ -4,8 +4,8 @@ import type {
   DeviceSettings,
   DriverSetting,
   LoginCredentials,
-  SettingValue,
   Settings,
+  SettingValue,
 } from '../types'
 
 async function onHomeyReady(Homey: Homey): Promise<void> {
