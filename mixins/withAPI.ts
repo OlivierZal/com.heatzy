da5c8c185d15b35f2d75f8a2514321739f8c3f98
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2023-11-22 00:07:02 +0100

    improve

--- mixins/withAPI.ts
+++ mixins/withAPI.ts
@@ -111,10 +111,7 @@ export default function withAPI<T extends HomeyClass>(base: T): APIClass & T {
           },
           Duration.fromObject({ minutes: 1 }).as('milliseconds'),
         )
-        const loggedIn: boolean = await (this.homey.app as HeatzyApp).login(
-          undefined,
-          false,
-        )
+        const loggedIn: boolean = await (this.homey.app as HeatzyApp).login()
         if (loggedIn && error.config) {
           return this.api.request(error.config)
         }
