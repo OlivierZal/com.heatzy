87c42d976b6956486b5fce78df39b0945657a658
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-02-03 02:20:56 +0100

    improve API logs

--- mixins/withAPI.ts
+++ mixins/withAPI.ts
@@ -14,6 +14,7 @@ import axios, {
   type AxiosInstance,
   type AxiosResponse,
   type InternalAxiosRequestConfig,
+  isAxiosError,
 } from 'axios'
 import type HeatzyApp from '../app'
 
@@ -52,12 +53,49 @@ const getAPIErrorMessage = (error: AxiosError): string => {
 
 export const getErrorMessage = (error: unknown): string => {
   let errorMessage = String(error)
-  if (axios.isAxiosError(error)) {
+  if (isAxiosError(error)) {
     errorMessage = getAPIErrorMessage(error)
   } else if (error instanceof Error) {
     errorMessage = error.message
   }
   return errorMessage
+}
+
+const getAPILogs = (
+  object: AxiosError | AxiosResponse | InternalAxiosRequestConfig,
+  message?: string,
+): string => {
+  const isError = isAxiosError(object)
+  const isResponse = Boolean(
+    (!isError && 'status' in object) || (isError && 'response' in object),
+  )
+  const config: InternalAxiosRequestConfig = (
+    isResponse || isError
+      ? (object as AxiosError | AxiosResponse).config
+      : object
+  ) as InternalAxiosRequestConfig
+  let response: AxiosResponse | null = null
+  if (isResponse) {
+    response = isError ? object.response ?? null : (object as AxiosResponse)
+  }
+  return [
+    `API ${isResponse ? 'response' : 'request'}:`,
+    config.method?.toUpperCase(),
+    config.url,
+    config.params,
+    isResponse ? response?.headers : config.headers,
+    response?.status,
+    (isResponse ? (object as AxiosResponse) : config).data,
+    message,
+  ]
+    .filter(
+      (log: number | object | string | undefined) => typeof log !== 'undefined',
+    )
+    .map((log: number | object | string): number | string =>
+      // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+      typeof log === 'object' ? JSON.stringify(log, null, 2) : log,
+    )
+    .join('\n')
 }
 
 // eslint-disable-next-line max-lines-per-function
@@ -120,16 +158,12 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
       const updatedConfig: InternalAxiosRequestConfig = { ...config }
       updatedConfig.headers['X-Gizwits-User-token'] =
         this.getHomeySetting('token') ?? ''
-      this.log(
-        'Sending request:',
-        updatedConfig.url,
-        updatedConfig.method === 'post' ? updatedConfig.data : '',
-      )
+      this.log(getAPILogs(updatedConfig))
       return updatedConfig
     }
 
     private handleResponse(response: AxiosResponse): AxiosResponse {
-      this.log('Received response:', response.config.url, response.data)
+      this.log(getAPILogs(response))
       return response
     }
 
@@ -138,7 +172,7 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
       error: AxiosError,
     ): Promise<AxiosError> {
       const errorMessage: string = getAPIErrorMessage(error)
-      this.error(`Error in ${type}:`, error.config?.url, errorMessage)
+      this.error(getAPILogs(error), errorMessage)
       const app: HeatzyApp = this.homey.app as HeatzyApp
       if (
         error.response?.status === HTTP_STATUS_BAD_REQUEST &&
