36148b82de9883f3fa2affde9b58bd4470eb9257
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-02-04 00:27:39 +0100

    improve

--- mixins/withAPI.ts
+++ mixins/withAPI.ts
@@ -132,13 +132,13 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
         (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig =>
           this.handleRequest(config),
         async (error: AxiosError): Promise<AxiosError> =>
-          this.handleError('request', error),
+          this.handleError(error),
       )
       this.api.interceptors.response.use(
         (response: AxiosResponse): AxiosResponse =>
           this.handleResponse(response),
         async (error: AxiosError): Promise<AxiosError> =>
-          this.handleError('response', error),
+          this.handleError(error),
       )
     }
 
@@ -157,10 +157,7 @@ const withAPI = <T extends HomeyClass>(base: T): APIClass & T =>
       return response
     }
 
-    private async handleError(
-      type: 'request' | 'response',
-      error: AxiosError,
-    ): Promise<AxiosError> {
+    private async handleError(error: AxiosError): Promise<AxiosError> {
       const apiCallData: string[] = getAPICallData(error)
       this.error(apiCallData.join('\n'))
       const app: HeatzyApp = this.homey.app as HeatzyApp
