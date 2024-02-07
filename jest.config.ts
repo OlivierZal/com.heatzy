11e0bd0718f8a524a43b9504bc186b9a9bbee210
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-02-07 14:24:53 +0100

    better

--- jest.config.ts
+++ jest.config.ts
@@ -4,8 +4,10 @@ const config: Config = {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  modulePathIgnorePatterns: ['<rootDir>/.homeybuild/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/.homeybuild/', '<rootDir>/node_modules/'],
 }
 
 export default config
