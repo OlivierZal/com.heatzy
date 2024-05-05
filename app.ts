3dd0bf08a42f996fb7a703484dbc211df351f47c
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-05-05 14:27:05 +0200

    improve

--- app.ts
+++ app.ts
@@ -4,7 +4,14 @@ import HeatzyAPI from '@olivierzal/heatzy-api'
 import { Settings as LuxonSettings } from 'luxon'
 
 export = class HeatzyApp extends App {
-  public readonly heatzyAPI = new HeatzyAPI(this.homey.settings, this)
+  public readonly heatzyAPI = new HeatzyAPI(this.homey.settings, {
+    error: (...args): void => {
+      this.error(...args)
+    },
+    log: (...args): void => {
+      this.log(...args)
+    },
+  })
 
   public override async onInit(): Promise<void> {
     LuxonSettings.defaultLocale = this.homey.i18n.getLanguage()
