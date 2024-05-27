52d7b8646d0d22117f96b0d3ad7bc071ec7cf1d6
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2024-05-28 00:44:46 +0200

    better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -275,7 +275,7 @@ export default tsEslint.config(
     ...tsEslint.configs.disableTypeChecked,
     languageOptions: {
       parserOptions: {
-        project: 'tsconfig.mjs.json',
+        project: 'tsconfig.checkjs.json',
       },
     },
   },
