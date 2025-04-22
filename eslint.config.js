b20753882048f2d6c3ae35d8647cccf76b0f6ac9
Olivier Zalmanski <88216225+OlivierZal@users.noreply.github.com>
2025-04-22 21:48:04 +0200

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -99,6 +99,7 @@ const moduleSortOptions = {
 
 const namedSortOptions = {
   ignoreAlias: true,
+  newlinesBetween: 'never',
 }
 
 const objectSortOptions = {
@@ -355,15 +356,15 @@ const config = defineConfig([
       'perfectionist/sort-named-exports': [
         'error',
         {
-          ...namedSortOptions,
           ...exportSortOptions,
+          ...namedSortOptions,
         },
       ],
       'perfectionist/sort-named-imports': [
         'error',
         {
-          ...namedSortOptions,
           ...importNamedSortOptions,
+          ...namedSortOptions,
         },
       ],
       'perfectionist/sort-object-types': ['error', typeLikeSortOptions],
